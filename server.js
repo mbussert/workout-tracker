9765923e66097e32be80e5ef2aa7e2485e5100b6
Mike Bussert <mbussert@gmail.com>
2021-05-18 16:34:08 -0400

    Deploying

--- server.js
+++ server.js
@@ -10,16 +10,18 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
-  useNewUrlParser: true,
-  useFindAndModify: false,
-  useCreateIndex: true,
-  useUnifiedTopology: true,
-});
+mongoose.connect(
+  process.env.MONGODB_URI || "mongodb://localhost/workoutTrackerDB",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  }
+);
 
 // routes
 app.use(require("./routes/api.js"));
-
 
 app.listen(PORT, () => {
   console.log(
